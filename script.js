024364c4072652e8f7ee81ac21486d129e277b6c
Zik9k <zirov6411@gmail.com>
2026-08-28 19:59:27 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 // Nexus Client website.
 // Downloading is handled by normal HTML links.
-// No JavaScript is used to block or redirect downloads.
+// No JavaScript is used for the download button.
